ff0ea073aff2ec986e26154239a355becb92ff10
crowec <connie81095@orange.fr>
2016-06-01 16:38:52 +0100

    test

--- test.py
+++ test.py
@@ -1,34 +1,31 @@
 from kaltura_lib.KalturaClient import *
 
-<<<<<<< HEAD
+from secret import admin_secret
+
 partnerID = 1817881
-admin_secret = "1a7227978d8228dde2a574fac2c9b371"
 userID = "kamelia.zhelyazkova@ed.ac.uk"
-ks_type = 2
-expiry = 360
-privileges = ""
-=======
-partnerID =
-admin_secret =
-userID =
-ks_type =
-expiry =
-privileges =
->>>>>>> 8dff3088c950601f606248c6d7761ed6bc51f048
-
-
+ks_type = 0
 
 config = KalturaConfiguration(partnerID)
 # where 123 is your partner ID
 
-config.serviceUrl = "http://devtests.kaltura.co.cc/"
+config.serviceUrl = "http://www.kaltura.com"
 # if you want to communicate with a Kaltura server which is
 #    other than the default http://www.kaltura.com
 
 client = KalturaClient(config)
 
-ks = client.generateSession(admin_secret, userID, ks_type, partnerID, expiry, privileges)
-
+ks = client.generateSession(admin_secret, userID, ks_type, partnerID)
+print ks
 client.setKs(ks)
 
-print("DONE!")
+
+entryId = "1_yacymbd7";
+# a known ID of media entry that you have
+
+try:
+    mediaEntry = client.media.get(entryId)
+    print mediaEntry.getName()
+except Base.KalturaException, e:
+    print "could not get entry from Kaltura. Reason: %s" % repr(e)
+
